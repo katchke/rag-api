d40edba406a903e01c34522aa305beb1c2c15fd9
Rahul Kamath <rahulkamath91@gmail.com>
2024-11-04 00:37:27 +0530

    delete files

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from dotenv import load_dotenv, find_dotenv
-
-
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    env_file = find_dotenv('.env')
-    load_dotenv(env_file)
