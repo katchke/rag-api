99ba10075ec014adc59593df2c5b8a70676d4363
Rahul Kamath <rahulkamath91@gmail.com>
2024-11-04 00:38:26 +0530

    update db init

--- docker/db/init.sh
+++ docker/db/init.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
-export PGUSER=ai_chatbot_admin
 psql <<- EOSQL
     CREATE USER $POSTGRES_USER;
     CREATE DATABASE $POSTGRES_DB;
+    \c $POSTGRES_DB
+    CREATE EXTENSION vector;
+    CREATE TABLE $GSCHOLAR_TABLE (
+        title TEXT,
+        link VARCHAR(2048) PRIMARY KEY,
+        citation_count INT,
+        authors TEXT,
+        content TEXT,
+        embedding vector(1536)
+    );
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
 EOSQL
