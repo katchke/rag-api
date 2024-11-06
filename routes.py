734b850670bd7d7db2fec20dbfcb5f320f386ed5
Rahul Kamath <rahulkamath91@gmail.com>
2024-11-06 14:00:02 +0530

    delete routes

--- routes.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Api
-from presentations.example import Example
-def routes(app):
-
-    api = Api(app)
-    api.prefix = '/v1'
-    api.add_resource(Example, '/examples/<example_id>', endpoint='example_get')
